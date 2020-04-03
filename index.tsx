4661e8f04da528eec208ead8c4e9909b9ffab51e
Hillkim <hillkim@protonmail.com>
2020-04-03 19:39:21 +0300

    Add dev entry file

---
+++ index.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import ReactFullMenu from "./src";
+
+const App = () => <ReactFullMenu />;
+
+ReactDOM.render(<App />, document.getElementById("example"));

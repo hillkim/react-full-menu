1a55962632478c3f528513e225d2101794389e28
Hillkim <hillkim@protonmail.com>
2020-04-04 15:22:52 +0300

    remove default wite color

--- index.tsx
+++ index.tsx
@@ -3,6 +3,6 @@ import * as ReactDOM from "react-dom";
 
 import ReactFullMenu from "./src";
 
-const App = () => <ReactFullMenu linkColor="green" />;
+const App = () => <ReactFullMenu linkColor="blue" />;
 
 ReactDOM.render(<App />, document.getElementById("example"));

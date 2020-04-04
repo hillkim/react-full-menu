406e90200877f9375c06adad91f29c9f78e79761
Hillkim <hillkim@protonmail.com>
2020-04-04 15:32:39 +0300

    :rocket: build and edit publish details

---
+++ lib/Icons.d.ts
@@ -0,0 +1,8 @@
+import React from "react";
+export declare const CloseIcon: ({ color, }: {
+    color: string;
+}) => React.ReactElement<SVGAElement, string | ((props: any) => React.ReactElement<any, string | any | (new (props: any) => React.Component<any, any, any>)> | null) | (new (props: any) => React.Component<any, any, any>)>;
+export declare const MenuIcon: ({ color, }: {
+    color: string;
+}) => React.ReactElement<SVGAElement, string | ((props: any) => React.ReactElement<any, string | any | (new (props: any) => React.Component<any, any, any>)> | null) | (new (props: any) => React.Component<any, any, any>)>;
+//# sourceMappingURL=Icons.d.ts.map
--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,6 +1,38 @@
 import React from "react";
-interface RFMProps {
+import "./menu.css";
+/**
+ * returns a menu link to be rendered
+ */
+declare type MenuLink = {
+    /**
+     * path where the link points to
+     */
+    to: string;
+    /**
+     * link text to be rendered
+     */
+    text: string;
+    /**
+     * custom handler
+     */
+    onClick?: (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => void;
+};
+interface MenuProps {
+    /**
+     * html background `colors` or gradient color to be rendered
+     */
+    background?: string;
+    /**
+     * html `link color` to be rendered
+     */
+    linkColor?: string;
+    links?: MenuLink[];
+    customLinkRenderer?: React.ReactNode;
+    /**
+     *  renders the selected font-family defaults to `'"Autour One", cursive'`
+     */
+    fontFamily?: string;
 }
-declare const ReactFullMenu: React.FC<RFMProps>;
+declare const ReactFullMenu: React.FC<MenuProps>;
 export default ReactFullMenu;
 //# sourceMappingURL=index.d.ts.map
